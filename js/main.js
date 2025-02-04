3e4ba1995cbed7900240607d5588aa4364d6e23e
howardosborne <hosborne@hotmail.com>
2025-02-04 17:46:48 +0000

    updated

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ function isMarkerInsidePolygon(marker) {
     })
     return inside;
 };
-
+/*
 async function getLatestCSOInfo() {
     let url = "https://script.google.com/macros/s/AKfycbx5I8sChfkuxusDl29yafXamEAXGGU9AyLp-RS_LY8tcg6ZvBOr5G3rKsor0WfvOkipzw/exec"
     const response = await fetch(url);
@@ -85,6 +85,73 @@ async function getLatestCSOInfo() {
         layerControl.addOverlay(inMaintenanceCSOs, `CSOs in maintenance (${inMaintenanceCSOsCount})`);
     }
 }
+*/
+
+async function getLatestCSOInfo() {
+    let url = "https://services3.arcgis.com/VCOY1atHWVcDlvlJ/arcgis/rest/services/stream_service_outfall_locations_view/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"
+    const response = await fetch(url);
+    if(response.status == 200){
+        var CSOs = new L.LayerGroup();
+        var dischargingCSOs = new L.LayerGroup();
+        var offlineCSOs = new L.LayerGroup();
+        var inMaintenanceCSOs = new L.LayerGroup();
+        var CSOsCount = 0, dischargingCSOsCount = 0, offlineCSOsCount = 0, inMaintenanceCSOsCount = 0;
+        const responseJson = await response.json();
+        let result = responseJson["features"];
+        CSOsCount = result.length;
+        result.forEach(element => {
+            let csoColor = "rgb(50, 100, 0)";
+            if(element.properties.status == 1){csoColor =  "rgb(100, 50, 0)";}
+            let marker = L.circleMarker([element.properties.Latitude, element.properties.Longitude],{radius:4,color:csoColor});
+            marker.bindTooltip(decodeURI(element.properties.Id));
+            marker.properties = element;
+            marker.addEventListener('click', _CsoMarkerOnClick);
+            marker.addTo(CSOs);
+            /*
+            let badge = "";
+            if(!element.properties){
+                offlineCSOsCount ++;
+                marker.addTo(offlineCSOs);
+                badge += ` <span class="badge text-bg-warning">offline</span>`;
+            }
+            if(element.is_in_maintenance){
+                inMaintenanceCSOsCount ++;
+                marker.addTo(inMaintenanceCSOs);
+                badge += ` <span class="badge text-bg-warning">in maintenance</span>`;
+            }
+            if(element.is_discharging){
+                dischargingCSOsCount ++;
+                marker.addTo(dischargingCSOs);
+                marker.bindPopup(`<div class="card">
+                    <h6>${element.site_name}${badge}</h6>
+                <ul class="list-group list-group-flush">
+                <li class="list-group-item">Started: ${element.recent_discharge.started}</li>
+                <li class="list-group-item">Duration: ${element.recent_discharge.duration_mins} mins</li>
+                <li class="list-group-item">Discharge to: ${element.receiving_water_or_environment}</li>
+			    <li class="list-group-item">Is overflow expected?: ${element.is_overflow_expected}</li>
+                </ul>
+                </div>`);
+            } 
+            else{
+                marker.bindPopup(`
+                <div class="card">
+                    <h6>${element.site_name}${badge}</h6>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Last discharge: ${element.recent_discharge.started}</li>
+                        <li class="list-group-item">Duration: ${element.recent_discharge.duration_mins} mins</li>
+                    <li class="list-group-item">Discharges to: ${element.receiving_water_or_environment}</li>
+                    </ul>
+                </div>`);                
+            }
+            */
+        });
+        layerControl.addOverlay(CSOs, `Anglian CSOs (${CSOsCount})`);
+        //layerControl.addOverlay(dischargingCSOs, `CSOs discharging (${dischargingCSOsCount})`);
+        //layerControl.addOverlay(offlineCSOs, `CSOs offline (${offlineCSOsCount})`);
+        //layerControl.addOverlay(inMaintenanceCSOs, `CSOs in maintenance (${inMaintenanceCSOsCount})`);
+    }
+}
+
 async function getRainfallData(){
     let url = "https://check-for-flooding.service.gov.uk/api/rainfall.geojson"
     const response = await fetch(url);
@@ -100,20 +167,9 @@ async function getRainfallData(){
             let marker = L.circleMarker([element.geometry.coordinates[1],element.geometry.coordinates[0]],{radius:4,color:rainfallColor});
             marker.bindTooltip(decodeURI(element.properties.station_name));
             marker.properties = element;
-            //marker.addEventListener('click', _starterMarkerOnClick);
+            marker.addEventListener('click', _rainfallMarkerOnClick);
             marker.addTo(rainfall);
             let badge = "";
-            marker.bindPopup(`
-                <div class="card">
-                    <h6>${element.properties.station_name}${badge}</h6>
-                    <ul class="list-group list-group-flush">
-				        <li class="list-group-item">"value": ${element.properties.value}</li>
-				        <li class="list-group-item">"value_timestamp": ${element.properties.value_timestamp}</li>
-				        <li class="list-group-item">"day_total": ${element.properties.day_total}</li>
-				        <li class="list-group-item">"six_hr_total": ${element.properties.six_hr_total}</li>
-				        <li class="list-group-item">"one_hr_total": ${element.properties.one_hr_total}</li>
-                    </ul>
-                </div>`);
             }
             catch(err){
                 console.log(err.message);
@@ -123,6 +179,43 @@ async function getRainfallData(){
     }
     
 }
+
+function _rainfallMarkerOnClick(e){
+    let output = `
+    <h6>${e.sourceTarget.properties.station_name}</h6>
+                    <ul class="list-group list-group-flush">
+				        <li class="list-group-item">"value": ${e.sourceTarget.properties.value}</li>
+				        <li class="list-group-item">"value_timestamp": ${e.sourceTarget.properties.value_timestamp}</li>
+				        <li class="list-group-item">"day_total": ${e.sourceTarget.properties.day_total}</li>
+				        <li class="list-group-item">"six_hr_total": ${e.sourceTarget.properties.six_hr_total}</li>
+				        <li class="list-group-item">"one_hr_total": ${e.sourceTarget.properties.one_hr_total}</li>
+                    </ul>`;
+
+    document.getElementById("offcanvas-body").innerHTML	= output;
+    var myOffcanvas = document.getElementById('offcanvas');
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+    bsOffcanvas.show();	    
+}
+
+function _CsoMarkerOnClick(e){
+    
+    let output = `
+        <h6>${e.sourceTarget.properties.properties.Id}</h6>
+                    <ul class="list-group list-group-flush">
+				        <li class="list-group-item">"Status": ${e.sourceTarget.properties.properties.Status}</li>
+                        <li class="list-group-item">"LastUpdated": ${new Date(e.sourceTarget.properties.properties.LastUpdated).toLocaleString('en-GB')}</li>
+				        <li class="list-group-item">"StatusStart": ${new Date(e.sourceTarget.properties.properties.StatusStart).toLocaleString('en-GB')}</li>
+				        <li class="list-group-item">"LatestEventStart": ${new Date(e.sourceTarget.properties.properties.LatestEventStart).toLocaleString('en-GB')}</li>
+				        <li class="list-group-item">"LatestEventEnd": ${new Date(e.sourceTarget.properties.properties.LatestEventEnd).toLocaleString('en-GB')}</li>
+                        <li class="list-group-item">"ReceivingWaterCourse": ${e.sourceTarget.properties.properties.ReceivingWaterCourse}</li>
+                    </ul>
+        `;
+    document.getElementById("offcanvas-body").innerHTML	= output;
+    var myOffcanvas = document.getElementById('offcanvas');
+    var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
+    bsOffcanvas.show();	    
+}
+
 async function getStationData(){
     //http://environment.data.gov.uk/flood-monitoring/id/stations?parameter=rainfall&lat=51.48&long=-2.77&dist=10
     let url = "./data/stations.json"
@@ -147,6 +240,14 @@ async function getStationData(){
         layerControl.addOverlay(stations, `Water level monitoring: (${stationCount})`);
     }
     
+}
+
+function _catchmentMarkerOnClick(e){
+    let uri = e.layer.feature.properties.uri; 
+    document.getElementById("toast-body").innerHTML	= `<a href="${uri.replace("/so/","/")}" target="_blank" class="card-link">${e.layer.feature.properties.name}</a>`;
+    const toastElement = document.getElementById('liveToast');
+    const toast = bootstrap.Toast.getOrCreateInstance(toastElement);
+    toast.show();  
 }
 
 function _stationMarkerOnClick(e){
@@ -240,25 +341,23 @@ async function addWaterbody(sourceData,name){
             else{return {color: lookup[name].color, weight:1};}
         }
     })
-    catchmentLayer.bindPopup(function (layer) {
+    catchmentLayer.bindTooltip(function (layer) {
         let uri = layer.feature.properties.uri;
-        let pop = `<div class="card">
-        <div class="card-title">
-        <h5 class="card-title">${layer.feature.properties.name}</h5>
-        <h6 class="card-subtitle mb-2 text-body-secondary"><a href="${lookup[name].url}" target="_blank" class="card-link">${name}</a></h6>
-        <a href="${uri.replace("/so/","/")}" target="_blank" class="card-link">Waterbody Details (EA)</a>
-        <!--<ul class="list-group list-group-flush">
+        let pop = `>${layer.feature.properties.name}
+        <!--
+        <a href="${uri.replace("/so/","/")}" target="_blank" class="card-link">EA details</a>
+        <ul class="list-group list-group-flush">
         <li class="list-group-item"><a class="button" href="${uri.replace("/so/","/")}" target="_blank">Environment Agency info</a></li>
         <li class="list-group-item"><a href="${lookup[name].url}" target="_blank">Catchment partnership</a></li>
         <li class="list-group-item"><a class="button" onclick="showClassifications('${layer.feature.properties.id}')">Classifications</a></li>
         <li class="list-group-item"><a class="button" onclick="showObjectives('${layer.feature.properties.id}')">Objectives</a></li>
         <li class="list-group-item"><a class="button" onclick="showProtectedAreas('${layer.feature.properties.id}')">Protected</a></li>
         <li class="list-group-item"><a class="button" onclick="showChallenges('${layer.feature.properties.id}')">Challenges</a></li>
-        </ul>-->
-        </div>`;
+        </ul>-->`;
         return pop;
     })
     //catchmentLayer.bindTooltip(lookup[name].name)
+    catchmentLayer.addEventListener('click', _catchmentMarkerOnClick);
     catchmentLayer.eachLayer(lay=> {polys.push(lay)});
     layerControl.addOverlay(catchmentLayer, lookup[name].name);
     catchmentLayer.addTo(map);
@@ -266,16 +365,13 @@ async function addWaterbody(sourceData,name){
   
 var server = "https://script.google.com/macros/s/AKfycbzplxYBoOcR9IPskJHrtIEs8TnLtX8iAPibQAVaQCOJNScwAtYz51HHiu8Uhwb7XSy54g/exec";
 var lookup = {
-    "UBOCP":{name:"Upper Bedford Ouse Catchment Partnership",url:"https://ubocp.org.uk/",color:"rgb(150, 150, 150)"},
-    "CamEO":{name:"Cambridge Ely Ouse",url:"https://www.cameopartnership.org/",color:"rgb(125, 200, 150)"},
-    "WCP":{name:"Water Care Partnership",url:"https://www.cambsacre.org.uk/water-care-catchment-partnership/",color:"rgb(1, 100, 100)"}	
+    "UBOCP":{name:"Upper Bedford Ouse Catchment Partnership",url:"https://ubocp.org.uk/",color:"rgb(81, 34, 34)"},
+    "CamEO":{name:"Cambridge Ely Ouse",url:"https://www.cameopartnership.org/",color:"rgb(2, 120, 41)"},
+    "WCP":{name:"Water Care Partnership",url:"https://www.cambsacre.org.uk/water-care-catchment-partnership/",color:"rgb(3, 163, 163)"}	
 }
 
 function loadMap(){
     map = L.map('map').setView([52.3322, -0.2773], 9);
-    //const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
-    //var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'}).addTo(map);
-    //var Esri_WorldTopoMap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'}).addTo(map);
     var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
     var img = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
     var top = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'});
